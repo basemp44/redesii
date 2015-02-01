087086cbd9a7c503552c33d8534dacdad6493579
basemp44 <dos314erre@hotmail.com>
2015-02-01 16:28:02 +0100

    Nueva base para trabajar

--- nombreproyecto/include/ircisvalidfunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IRCISVALIDFUNCTIONS_H_
-#define _IRCISVALIDFUNCTIONS_H_
-
-long IRC_IsValid(char *tested, int maxl, unsigned int *ip, long type);
-void IRC_ToLower(char *str);
-void IRC_ToUpper(char *str);
-
-#endif
